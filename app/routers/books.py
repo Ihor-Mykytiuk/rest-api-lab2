f00b59f6079141783918124232499771487ffea7
Ihor Mykytiuk <igor14vn@gmail.com>
2025-03-07 17:51:45 +0200

    remove duplication

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from app.models import Book, books
+from typing import List
 
 router = APIRouter(
     prefix="/books",
@@ -8,20 +9,25 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def get_books():
-    return books
-
-
-@router.get("/{book_id}")
-async def get_book(book_id: str):
+async def get_book_by_id(book_id: str) -> Book:
     book = next((book for book in books if book.id == book_id), None)
     if book is None:
         raise HTTPException(status_code=404, detail="Book not found")
     return book
 
 
-@router.post("/", status_code=201)
+@router.get("/", response_model=List[Book])
+async def get_books():
+    return books
+
+
+@router.get("/{book_id}", response_model=Book)
+async def get_book(book_id: str):
+    book = await get_book_by_id(book_id)
+    return book
+
+
+@router.post("/", status_code=201, response_model=Book)
 async def add_book(book: Book):
     books.append(book)
     return book
@@ -29,8 +35,6 @@ async def add_book(book: Book):
 
 @router.delete("/{book_id}", status_code=204)
 async def delete_book(book_id: str):
-    book = next((book for book in books if book.id == book_id), None)
-    if book is None:
-        raise HTTPException(status_code=404, detail="Book not found")
+    book = await get_book_by_id(book_id)
     books.remove(book)
-    return {"message": "Book deleted"}
+    return
