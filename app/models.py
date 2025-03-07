7f543224f2957a08e0027dce6ea6765c04cbfe1c
Ihor Mykytiuk <igor14vn@gmail.com>
2025-03-07 17:56:34 +0200

    add validations for Book model

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel, Field
 import uuid
 from typing import List
+from datetime import datetime
 
 
 class Book(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()))
-    title: str
-    author: str
-    published_year: int = Field(..., ge=1000, le=2100)
+    title: str = Field(..., max_length=100)
+    author: str = Field(..., max_length=255)
+    published_year: int = Field(..., ge=1000, le=datetime.now().year)
 
 
 books: List[Book] = []
